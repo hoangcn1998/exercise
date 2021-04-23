cf6a4e92e8ba295987c9cca00edda0657902c9d5
Ngoc Hoang <hoangcn1998@gmail.com>
2021-04-24 01:12:30 +0700

    New code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
-  console.log("asdf");
+    const listImages = $(".slider img");
+    let index = 0;
+
+    function next() {
+      $(".slider__control.next").click(function() {
+        if(index < listImages.length - 1) {
+          index++
+        } else {
+          index = 0;
+          for(let i = 0; i < listImages.length; i++) {
+            $(listImages[i]).css({"transform" : ""});
+          }
+        }
+
+        $(listImages[index]).css({
+          "transition" : "all 0.5s",
+          "transform" : `translateX(-${100*index}%)`
+        })
+      })
+    }
+
+    function prev() {
+     
+    }
+
+  function main() {
+    next();
+    prev();
+  }
+
+  main();
 })
